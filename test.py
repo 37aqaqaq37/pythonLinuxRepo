7d56c71ec665286dcdd36e5ee7191e502767a506
Ammad <37aqaqaq37@gmail.com>
2019-11-17 12:20:00 +0000

    pylinuxrepo

--- test.py
+++ test.py
@@ -1,15 +1,17 @@
-#pycharm and github
+#! /bin/bash/ env Python3
 
 import random
+ls = []
 
 x = random.randint(1,10)
 i = input("Enter your name: ")
 print(f"The number {x} is assigned to {i}")
+ls.append(i)
 
 for l in range(5):
     print(l+1)
 print("This is my test code")
-ls = []
+
 for m in range(5):
 
     user = input("Enter a word: ")
