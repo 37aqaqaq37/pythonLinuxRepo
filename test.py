7b236900cf21451d30ef2090339f701b400749d3
Ammad <37aqaqaq37@gmail.com>
2019-11-17 12:12:30 +0000

    pylinuxrepo

--- test.py
+++ test.py
@@ -9,3 +9,10 @@ print(f"The number {x} is assigned to {i}")
 for l in range(5):
     print(l+1)
 print("This is my test code")
+ls = []
+for m in range(5):
+
+    user = input("Enter a word: ")
+    ls.append(user)
+
+print(ls)
